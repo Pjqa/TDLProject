378434588fe35144a7004d9b2d4f059768488de1
Pjqa <>
2021-02-10 16:48:56 +0000

    PTDL-8#created itemsrepo and controller, created the mapping for create

--- src/main/java/com/qa/projecttwo/controller/ItemsController.java
+++ src/main/java/com/qa/projecttwo/controller/ItemsController.java
@@ -1,5 +1,36 @@
 package com.qa.projecttwo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.projecttwo.dto.ItemsDto;
+import com.qa.projecttwo.persistence.domain.Items;
+import com.qa.projecttwo.service.ItemsService;
+
+@RestController
+@CrossOrigin
+@RequestMapping ("/items")
 public class ItemsController {
 
+	private ItemsService service;
+	
+	@Autowired
+	public ItemsController(ItemsService itemsService) {
+		super();
+		this.service = itemsService;
+	}
+	
+	//CREATE
+	@PostMapping("/create")
+	public ResponseEntity<ItemsDto> create(@RequestBody Items items){
+ 		ItemsDto created = this.service.create(items);
+		return new ResponseEntity<>(created, HttpStatus.CREATED);
+	}
+	
 }
