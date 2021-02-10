fa1264ee663e244aee5f3bc90252da425bd414a6
Pjqa <>
2021-02-10 17:24:53 +0000

    PTDL-11# Created the mapping for deleting an item in the itemscontroller

--- src/main/java/com/qa/projecttwo/controller/ItemsController.java
+++ src/main/java/com/qa/projecttwo/controller/ItemsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,4 +49,10 @@ public class ItemsController {
 	public ResponseEntity <ItemsDto> readSolo(@PathVariable Long id){
 		return ResponseEntity.ok(this.service.readById(id));
 	}
-}
+	
+	//DELETE
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity <ItemsDto> delete (@PathVariable Long id){
+		return this.service.delete(id)? new ResponseEntity <> (HttpStatus.NO_CONTENT): new ResponseEntity <> (HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	}
