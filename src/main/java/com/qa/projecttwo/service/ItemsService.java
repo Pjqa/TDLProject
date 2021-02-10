8311ea363512eee2d9e1bee8c324ae16fc6145a2
Pjqa <>
2021-02-10 16:38:03 +0000

    PTDL-8#created itemservice class and the create item funcation in it

--- src/main/java/com/qa/projecttwo/service/ItemsService.java
+++ src/main/java/com/qa/projecttwo/service/ItemsService.java
@@ -1,5 +1,34 @@
 package com.qa.projecttwo.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.projecttwo.dto.ItemsDto;
+import com.qa.projecttwo.persistence.domain.Items;
+import com.qa.projecttwo.persistence.repo.ItemsRepo;
+
+@Service
 public class ItemsService {
+	
+	private ItemsRepo repo;
+	
+	private ModelMapper mapper;
+	
+	private ItemsDto mapToDto (Items items) {
+		return this.mapper.map(items, ItemsDto.class);
+	}
+	
+	@Autowired
+	public ItemsService (ItemsRepo repo, ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+	
+	//CREATE
+	public ItemsDto create (Items items) {
+		return this.mapToDto(this.repo.save(items));
+	}
 
 }
