faef8781bb4d4fc1ac77fdb4549c9a6444f027d9
Pjqa <>
2021-02-11 17:46:55 +0000

    PTDL-6#Made the mapping to delete a todolist

--- src/main/java/com/qa/projecttwo/controller/ToDoListController.java
+++ src/main/java/com/qa/projecttwo/controller/ToDoListController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +53,13 @@ public class ToDoListController {
 	
 	//DELETE
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity <ToDoListDto> delete(@PathVariable Long id){
+	public ResponseEntity <ToDoListDto> delete (@PathVariable Long id){
 		return this.service.delete(id)? new ResponseEntity <> (HttpStatus.NO_CONTENT): new ResponseEntity <> (HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	//UPDATE
+	@PutMapping("/update/{id}")
+	public ResponseEntity <ToDoListDto> update (@PathVariable Long id, @RequestBody ToDoListDto toDoListDto){
+		return new ResponseEntity <> (this.service.update(toDoListDto, id),HttpStatus.ACCEPTED);
 	}
 }
