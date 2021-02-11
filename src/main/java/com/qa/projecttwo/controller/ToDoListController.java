495c01b4e086b099872d088d64d466fd86c53cf5
Pjqa <>
2021-02-11 17:35:42 +0000

    PTDL-7# Made the mapping to delete id for todolist

--- src/main/java/com/qa/projecttwo/controller/ToDoListController.java
+++ src/main/java/com/qa/projecttwo/controller/ToDoListController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,11 @@ public class ToDoListController {
 	@GetMapping ("/read/{id}")
 	public ResponseEntity <ToDoListDto> solo (@PathVariable Long id){
 		return ResponseEntity.ok(this.service.readById(id));
+	}
+	
+	//DELETE
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity <ToDoListDto> delete(@PathVariable Long id){
+		return this.service.delete(id)? new ResponseEntity <> (HttpStatus.NO_CONTENT): new ResponseEntity <> (HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
