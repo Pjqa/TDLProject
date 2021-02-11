e1bfcdebe601679559d3e37764f0f4dc16e01253
Pjqa <>
2021-02-11 18:10:07 +0000

    added the annotations

--- src/main/java/com/qa/projecttwo/persistence/domain/ToDoList.java
+++ src/main/java/com/qa/projecttwo/persistence/domain/ToDoList.java
@@ -3,10 +3,15 @@ package com.qa.projecttwo.persistence.domain;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +26,9 @@ public class ToDoList {
 	private long id;
 	@NotNull
 	private String name;
+	
+	@OneToMany(mappedBy = "toDoList", fetch = FetchType.EAGER)
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	
 	private List<Items> items;
 
