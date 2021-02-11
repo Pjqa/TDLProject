c9ff0a238fe2b33de3521eec31618f858b6a72de
Pjqa <>
2021-02-11 16:59:04 +0000

    PTDL-4# Made the create function for the todolist

--- src/main/java/com/qa/projecttwo/service/ToDoListService.java
+++ src/main/java/com/qa/projecttwo/service/ToDoListService.java
@@ -1,5 +1,34 @@
 package com.qa.projecttwo.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.projecttwo.dto.ToDoListDto;
+import com.qa.projecttwo.persistence.domain.ToDoList;
+
+@Service
 public class ToDoListService {
 
+	private ToDoListRepo repo;
+	
+	private ModelMapper mapper;
+	
+	private ToDoListDto mapToDto(ToDoList toDoList) {
+		return this.mapper.map(toDoList, ToDoListDto.class);
+	}
+	
+	@Autowired
+	public ToDoListService (ToDoListRepo repo, ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+	
+	//CREATE
+	public ToDoListDto create (ToDoList toDoList) {
+		return this.mapToDto(this.repo.save(toDoList));
+	}
+	
+	
 }
