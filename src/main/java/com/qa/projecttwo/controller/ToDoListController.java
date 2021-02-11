7932f9500bf02e1b0e1b91320d17cf34b5757ada
Pjqa <>
2021-02-11 17:06:16 +0000

    PTDL-4# Made the mapping for the create function

--- src/main/java/com/qa/projecttwo/controller/ToDoListController.java
+++ src/main/java/com/qa/projecttwo/controller/ToDoListController.java
@@ -1,5 +1,36 @@
 package com.qa.projecttwo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.projecttwo.dto.ToDoListDto;
+import com.qa.projecttwo.persistence.domain.ToDoList;
+import com.qa.projecttwo.service.ToDoListService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/toDoList")
 public class ToDoListController {
+	
+	private ToDoListService service;
+	
+	@Autowired
+	public ToDoListController(ToDoListService service) {
+		super();
+		this.service = service;
+	}
+	
+	//CREATE
+	@PostMapping("/create")
+	public ResponseEntity <ToDoListDto> create (@RequestBody ToDoList toDoList){
+		ToDoListDto created = this.service.create(toDoList);
+		return new ResponseEntity<>(created,HttpStatus.CREATED);
+	}
 
 }
